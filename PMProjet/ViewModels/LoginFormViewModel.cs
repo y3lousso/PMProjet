87bd94d3d5f55ce9e61f53b88af258286a497985
Thomas Faget <thomas.faget@hotmail.fr>
2018-04-21 11:30:17 -0400

    Add LoginFormViewModel view : print message on the login form

---
+++ PMProjet/ViewModels/LoginFormViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PMProjet.ViewModels
+{
+    public class LoginFormViewModel
+    {
+        public string Message { get; set; }
+        public string MessageColor { get; set; }
+    }
+}
