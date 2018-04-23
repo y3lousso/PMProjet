95b481add9a393e6b1576f721c87b02f4f18178f
Thomas Faget <thomas.faget@hotmail.fr>
2018-04-23 12:27:08 -0400

    Setup for Admin authorization and SignIn/SignOut pattern

--- PMProjet/Startup.cs
+++ PMProjet/Startup.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Server.IISIntegration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PMProjet.Authentication;
 using PMProjet.Models;
 
 namespace PMProjet
@@ -26,12 +34,29 @@ namespace PMProjet
             services.AddDbContext<MyDbContext>(options =>options.UseSqlServer(Configuration.GetConnectionString("MyDbContext")));
             services.AddSession();
             services.AddMvc();
+
+            // Authentication 
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
+                    options =>
+                    {
+                        options.AccessDeniedPath = "/Login/AccessDenied";
+                        options.LoginPath = "/Login/AccessDenied";
+                        options.LogoutPath = "/Login/Deconnexion";
+                    });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseSession();
+            app.UseAuthentication();
 
             if (env.IsDevelopment())
             {
